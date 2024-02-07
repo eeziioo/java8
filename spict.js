617fd85b0de0db92fdf41f6f2d09c3517e04d867
ummaar <155968282+eeziioo@users.noreply.github.com>
2024-02-07 15:05:19 +0500

    Add files via upload

---
+++ spict.js
@@ -0,0 +1,69 @@
+
+let savol1 = +prompt(`
+Apple kampaniyasi asoschisi kim?
+1) Stiv Jobs
+2) Jon lenon
+3) Pavel Durov
+4) Ilon Mask
+`)
+
+switch (savol1) {
+    case 1:
+        alert('To`g`ri')
+        break;
+    case 2:
+        alert('Notog`ri')
+        break;
+    case 3:
+        alert('Notog`ri')
+        break;
+    case 4:
+        alert('Notog`ri')
+        break;
+}
+
+let savol2 = +prompt(`
+JavaScpipt da qaysi o'zgaruvchi mavjud emas?
+1) Var
+2) Let
+3) Const
+4) Itn
+`)
+
+switch (savol2) {
+    case 1:
+        alert('Notog`ri')
+        break;
+    case 2:
+        alert('Notog`ri')
+        break;
+    case 3:
+        alert('Notog`ri')
+        break;
+    case 4:
+        alert('To`g`ri')
+        break;
+}
+
+let savol3 = +prompt(`
+HTML da qaysi element harfni qalin qilib beradi?
+1) <i></i>
+2) <em></em>
+3) <b></b>
+4) <p></p>
+`)
+
+switch (savol3) {
+    case 1:
+        alert('Notog`ri')
+        break;
+    case 2:
+        alert('Notog`ri')
+        break;
+    case 3:
+        alert('To`g`ri')
+        break;
+    case 4:
+        alert('Notog`ri')
+        break;
+}
